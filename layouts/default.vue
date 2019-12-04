855a35e684aa745b52bccc09fac832f6123e128e
Smit Patel <smitbharatkumar.patel@dcmail.ca>
2019-12-04 00:54:17 -0500

    Added style unscoped

--- layouts/default.vue
+++ layouts/default.vue
@@ -14,7 +14,7 @@
 import { FadeTransition } from "vue2-transitions";
 import AppHeader from "./AppHeader";
 import AppFooter from "./AppFooter";
-import AOS from "aos";
+
 export default {
   data() {
     return {};
@@ -25,11 +25,7 @@ export default {
     AppFooter
   },
   mounted() {
-    setTimeout(() => {
-      AOS.init({
-        disable: false
-      });
-    }, 300);
+
   },
   computed: {
     currentRoute(){
@@ -38,6 +34,32 @@ export default {
   },
 };
 </script>
+<style lang="scss">
+  .vue-notification {
+    animation:slide-left 0.6s cubic-bezier(.25,.46,.45,.94) forwards;
+    @apply text-primary-500 bg-white border-l-4 border-primary-300 border-solid text-base mt-10 p-3 shadow-lg;
+
+    .notification-title {
+        @apply text-gray-600;
+    }
+  
+  
+    &.warn {
+        @apply text-yellow-500 border-yellow-500;
+    }
+  
+    &.error {
+        @apply text-red-500 border-red-500;
+    }
+  
+    &.success {
+        @apply text-green-500 border-green-500;
+    }
+  }
+
+.slide-left{animation:slide-left .5s cubic-bezier(.25,.46,.45,.94) forwards}
+@keyframes slide-left{0%{transform:translateX(0px)}100%{transform:translateX(-50px)}}
+</style>
 <style lang="scss" scoped>
 .bg-main-page {
   background: transparent;
